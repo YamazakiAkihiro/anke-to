e5b1cd1ec1d5162901c933f4ee1740cb3efd5200
YamazakiAkihiro <74123417+YamazakiAkihiro@users.noreply.github.com>
2020-12-25 22:03:06 +0900

    ファイル一式

---
+++ input.php
@@ -0,0 +1,18 @@
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>課題テンプレート</title>
+</head>
+
+<body>
+    <form action="write.php" method="post">
+        お名前: <input type="text" name="name"><br>
+        EMAIL: <input type="text" name="mail"><br>
+        好きなもの: <input type="text" name="sukinamono"><br>
+        嫌いなもの: <input type="text" name="kirainamono"><br>
+        <input type="submit" value="送信">
+    </form>
+</body>
+
+</html>
